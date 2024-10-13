5d62846a50f2c92090c5c5f13cb1974b749d54f0
Bogdan Zhuravlov <arsbogdanus@gmail.com>
2024-10-13 18:04:17 +0300

    added languages

--- app/src/main/java/com/brz/lessontime/MainActivity.java
+++ app/src/main/java/com/brz/lessontime/MainActivity.java
@@ -7,12 +7,20 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 
+import com.brz.lessontime.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        initUi();
+        initListeners();
+
 
         // Установка цвета статус-бара
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
@@ -25,5 +33,19 @@ public class MainActivity extends AppCompatActivity {
                 decor.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
             }
         }
+    }
+
+    private void initUi() {
+        binding.mainMonday.dayTitle.setText(getString(R.string.monday));
+        binding.mainTuesday.dayTitle.setText(getString(R.string.tuesday));
+        binding.mainWednesday.dayTitle.setText(getString(R.string.wednesday));
+        binding.mainThursday.dayTitle.setText(getString(R.string.thursday));
+        binding.mainFriday.dayTitle.setText(getString(R.string.friday));
+        binding.mainSaturday.dayTitle.setText(getString(R.string.saturday));
+        binding.mainSunday.dayTitle.setText(getString(R.string.sunday));
+    }
+
+    private void initListeners() {
+
     }
 }
