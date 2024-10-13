32f49b271e0e72cb585b6a6b852fa6142381f498
Bogdan Zhuravlov <arsbogdanus@gmail.com>
2024-10-13 17:27:25 +0300

    added view binding

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,6 +29,10 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
@@ -39,4 +43,5 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+    implementation("androidx.core:core-ktx:1.13.1")
 }
