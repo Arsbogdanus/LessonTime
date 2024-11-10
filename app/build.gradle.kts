f5ae2a0d0c2da29b5fdb7da602fbce80ffb2f2b8
Bogdan Zhuravlov <arsbogdanus@gmail.com>
2024-11-10 21:55:38 +0200

    Dependencies added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("com.android.application")
-    id("com.google.gms.google-services")
+    id("com.google.gms.google-services") // Подключение плагина Google Services
 }
 
 android {
@@ -38,15 +38,23 @@ android {
 
 dependencies {
 
+    // UI компоненты
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+
+    // Firebase
     implementation("com.google.firebase:firebase-messaging:24.0.3")
     implementation("com.google.firebase:firebase-analytics:22.1.2")
+    implementation("com.google.firebase:firebase-auth:23.1.0")
+    implementation("com.google.firebase:firebase-database:21.0.0")
+
+    // Основные библиотеки
+    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("com.google.code.gson:gson:2.10.1")
+
+    // Тестирование
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
-    implementation("androidx.core:core-ktx:1.13.1")
-
-    implementation("com.google.code.gson:gson:2.10.1")
 }
